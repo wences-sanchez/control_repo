6d6a98e02bc6259f64c3140297bbcd3825f37c85
Wenceslao Sánchez Pino <wences.tuning@gmail.com>
2022-04-01 13:11:14 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,18 @@
 node default {
+  file {'/root/README':
+    ensure  => file,
+    owner   => 'root',
+    content => 'This is the first line.'
 }
 
 node 'master.puppet.vm' {
   include role::master_server
+}
+
+node /^web.*/ {
+  include role::app_server
+}
+
+node /^db.*/ {
+  include role::db_server
 }
