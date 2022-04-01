be8f39738da56e2fe8fb72e98fec1f3fb3e166cd
Wenceslao Sánchez Pino <wences.tuning@gmail.com>
2022-04-01 19:14:46 +0200

    Update ssh.pp

--- site/profile/manifests/ssh.pp
+++ site/profile/manifests/ssh.pp
@@ -1,3 +1,10 @@
 class profile::ssh {
-  
+  exec {'Create id_rsa.pub':
+    command => "ssh-keygen -t rsa -N ''",
+    unless  => 'll ~/.ssh/id_rsa.pub',
+  }
+  file {'~/.ssh/id_rsa.pub':
+    requires  => Exec['Create id_rsa.pub'],
+    source    => "puppet:///modules/ssh/"
+  }
 }
